397919a63d315b7317a611b16ab5d0e8e2294af4
SusLiu03 <susannl5@uci.edu>
2023-11-20 21:47:47 -0800

    praying

--- rust_script/src/main.rs
+++ rust_script/src/main.rs
@@ -31,40 +31,26 @@ use std::process::Command;
 use std::thread;
 
 fn main() {
+    // Set the RANACAPA_PORT environmental variable
     // Specify the path to R executable
     let r_executable_path = "/usr/bin/R"; // Adjust this path based on your actual setup
 
-    // Generate a comma-separated list of port numbers
-    let port_numbers: Vec<String> = (6407..6416).map(|port| port.to_string()).collect();
-    let ports_string = port_numbers.join(",");
-
-    // Update NGINX configuration with the new port numbers
-    let nginx_command = format!("sudo nginx -s reload -g \"set $thread_ports {};\"", ports_string);
-
-    // Execute the NGINX reload command
-    Command::new("sh")
-        .arg("-c")
-        .arg(&nginx_command)
-        .spawn()
-        .expect("Failed to reload NGINX configuration");
-
     // Create 10 threads, each with a different port number
-    let threads: Vec<_> = (6407..6416)
-        .map(|port| {
-            let r_executable_path_clone = r_executable_path.to_string();
+    let threads: Vec<_> = (0..9)
+        .map(|i| {
+            let port = 6407 + i;
+            
+            env::set_var("RANACAPA_PORT", &port.to_string()); // Set your desired port number
             thread::spawn(move || {
-                // Set the RANACAPA_PORT environmental variable
-                env::set_var("RANACAPA_PORT", &port.to_string());
-
                 // Run the Shiny app using R with the specific port
-                let mut binding = Command::new(&r_executable_path_clone);
-                let command_r = binding.arg("-e").arg("library(ranacapa); ranacapa::runRanacapaApp()");
-                match command_r.status() {
-                    Ok(status) if status.success() => {
-                        println!("Shiny app started successfully on port {}.", port);
-                    }
-                    Ok(status) => eprintln!("Error starting Shiny app on port {}: {:?}", port, status),
-                    Err(e) => eprintln!("Error starting Shiny app on port {}: {}", port, e),
+            let mut binding = Command::new(&r_executable_path);
+            let command_r = binding.arg("-e").arg("library(ranacapa); ranacapa::runRanacapaApp()");
+            match command_r.status() {
+                Ok(status) if status.success() => {
+                    println!("Shiny app started successfully.");
+                },
+                Ok(status) => eprintln!("Error starting Shiny app: {:?}", status),
+                Err(e) => eprintln!("Error starting Shiny app: {}", e),
                 }
             })
         })
