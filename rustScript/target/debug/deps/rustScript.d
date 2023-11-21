d967871c0d52cc12951befdacdf8bb7d439d55bb
SusLiu03 <susannl5@uci.edu>
2023-11-20 18:41:13 -0800

    converted into cargo project

---
+++ rustScript/target/debug/deps/rustScript.d
@@ -0,0 +1,5 @@
+C:\Users\susan\rust-rstudio-multi-instance\rustScript\target\debug\deps\rustScript.exe: src\main.rs
+
+C:\Users\susan\rust-rstudio-multi-instance\rustScript\target\debug\deps\rustScript.d: src\main.rs
+
+src\main.rs:
---
+++ rustScript/target/debug/rustScript.d
@@ -0,0 +1,1 @@
+C:\Users\susan\rust-rstudio-multi-instance\rustScript\target\debug\rustScript.exe: C:\Users\susan\rust-rstudio-multi-instance\rustScript\src\main.rs
