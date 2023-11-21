3eefcdf1b8dda5f4b6bc864556574c29e4da8379
SusLiu03 <susannl5@uci.edu>
2023-11-20 19:02:10 -0800

    fixed temporary value dropped error

--- rustScript/src/main.rs
+++ rustScript/src/main.rs
@@ -9,10 +9,10 @@ fn main() {
     let r_executable_path = "/usr/bin/R";  // Adjust this path based on your actual setup
 
     // Run the Shiny app using R
-    let command_r = Command::new(&r_executable_path)
+    let binding = Command::new(&r_executable_path)
         .arg("-e")
         .arg("library(ranacapa); ranacapa::runRanacapaApp()");
-
+    let command_r = binding;
     match command_r.status() {
         Ok(status) if status.success() => {
             println!("Shiny app started successfully. Visit http://localhost:6407/ranacapa to access the app.");
