61e78830ecc16524512492ac2450b764609d436a
SusLiu03 <susannl5@uci.edu>
2023-12-01 18:17:36 -0800

    Only 15 groups

--- rust_script/src/main.rs
+++ rust_script/src/main.rs
@@ -1,79 +1,62 @@
-use std::env;
-use std::process::Command;
-use std::thread;
-use std::sync::mpsc;
-
-fn main() {
-    let r_executable_path = "/usr/bin/R"; // R executable path
-
-    // Create a channel for communication between threads
-    let (tx, rx) = mpsc::channel();
-
-    // Create 20 threads, each with a different port number
-    let threads: Vec<_> = (0..20)
-        .map(|i| {
-            let port = 6407 + i;
-            let tx = tx.clone();
-
-            thread::spawn(move || {
-                // Run the Shiny app using R with the specific port
-                let mut binding = Command::new(&r_executable_path);
-                let command_r = binding.arg("-e").arg("library(ranacapa); ranacapa::runRanacapaApp()");
-
-                match command_r.status() {
-                    Ok(status) if status.success() => {
-                        tx.send(format!("Shiny app started successfully on port {}", port)).unwrap();
-                    },
-                    Ok(status) => eprintln!("Error starting Shiny app: {:?}", status),
-                    Err(e) => eprintln!("Error starting Shiny app: {}", e),
-                }
-            })
-        })
-        .collect();
-
-    // Wait for all threads to finish and collect messages
-    for thread in threads {
-        thread.join().unwrap();
-    }
-
-    // Collect messages from threads
-    for received in rx {
-        println!("{}", received);
-    }
-}
-
-
 // use std::env;
 // use std::process::Command;
 // use std::thread;
 
 // fn main() {
+//     // Set the RANACAPA_PORT environmental variable
+//     let handle = thread::spawn(|| {
+//         env::set_var("RANACAPA_PORT", "6407");  // Set your desired port number
 
-//     let r_executable_path = "/usr/bin/R"; // R executable path
+//         // Specify the path to R executable
+//         let r_executable_path = "/usr/bin/R";  // Adjust this path based on your actual setup
 
-//     // Create 20 threads, each with a different port number
-//     let threads: Vec<_> = (0..20)
-//         .map(|i| {
-//             let port = 6407 + i;
-            
-//             env::set_var("RANACAPA_PORT", &port.to_string()); // Set your desired port number
-//             thread::spawn(move || {
-//                 // Run the Shiny app using R with the specific port
-//             let mut binding = Command::new(&r_executable_path);
-//             let command_r = binding.arg("-e").arg("library(ranacapa); ranacapa::runRanacapaApp()");
-//             match command_r.status() {
-//                 Ok(status) if status.success() => {
-//                     println!("Shiny app started successfully {}", port);
-//                 },
-//                 Ok(status) => eprintln!("Error starting Shiny app: {:?}", status),
-//                 Err(e) => eprintln!("Error starting Shiny app: {}", e),
-//                 }
-//             })
-//         })
-//         .collect();
+//         // Run the Shiny app using R
+//         let mut binding = Command::new(&r_executable_path);
+//         let command_r = binding.arg("-e").arg("library(ranacapa); ranacapa::runRanacapaApp()");
+//         match command_r.status() {
+//             Ok(status) if status.success() => {
+//                 println!("Shiny app started successfully. Visit http://localhost:6407/ranacapa to access the app.");
+//             },
+//             Ok(status) => eprintln!("Error starting Shiny app: {:?}", status),
+//             Err(e) => eprintln!("Error starting Shiny app: {}", e),
+//         }
+//     });
 
-//     // Wait for all threads to finish
-//     for thread in threads {
-//         thread.join().unwrap();
-//     }
+//     // Wait for the spawned thread to finish
+//     handle.join().unwrap();
 // }
+
+use std::env;
+use std::process::Command;
+use std::thread;
+
+fn main() {
+
+    let r_executable_path = "/usr/bin/R"; // R executable path
+
+    // Create 20 threads, each with a different port number
+    let threads: Vec<_> = (0..15)
+        .map(|i| {
+            let port = 6407 + i;
+            
+            env::set_var("RANACAPA_PORT", &port.to_string()); // Set your desired port number
+            thread::spawn(move || {
+                // Run the Shiny app using R with the specific port
+            let mut binding = Command::new(&r_executable_path);
+            let command_r = binding.arg("-e").arg("library(ranacapa); ranacapa::runRanacapaApp()");
+            match command_r.status() {
+                Ok(status) if status.success() => {
+                    println!("Shiny app started successfully {}", port);
+                },
+                Ok(status) => eprintln!("Error starting Shiny app: {:?}", status),
+                Err(e) => eprintln!("Error starting Shiny app: {}", e),
+                }
+            })
+        })
+        .collect();
+
+    // Wait for all threads to finish
+    for thread in threads {
+        thread.join().unwrap();
+    }
+}
